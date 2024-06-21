8099c8486cf84aeb6bab9aeecc9b8bc547b5d206
Jeffrey Gerrits <jeffreygerrits86@gmail.com>
2024-06-21 11:54:40 +0200

    Initial commit

---
+++ Design/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace Design
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, welcome to my software design and architecture.\nI made this repository so I can practise building small projects to demonstrate\n" +
+                " basic design principles. I hope you enjoy.");
+        }
+    }
+}
